57eb6177d8b38fd2a16194c08974da7d920ae850
Sahil <sk4386840@gmail.com>
2025-07-08 15:24:21 +0530

    create login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -14,10 +14,10 @@ export const signup = async (req, res) => {
     }
     const hashedPass = await bcrypt.hash(password, 10);
     const newUser = new User({ name, email, password: hashedPass });
-    await newUser.save(); 
+    await newUser.save();
 
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d"
+      expiresIn: "7d",
     });
     res.cookie("token", token, {
       httpOnly: true,
@@ -26,15 +26,42 @@ export const signup = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     const mailOptions = {
-        from: process.env.SENDER_EMAIL,
-        to: email,
-        subject: "Welcome to Instance",
-        text: "Your account on Instance has been created!"
-    }
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome to Instance",
+      text: "Your account on Instance has been created!",
+    };
     await transporter.sendMail(mailOptions);
     return res.json({ success: true, message: "Signup successful!" });
   } catch (error) {
     console.log("Error in signup controller: ", error.message);
     return res.json({ message: "Server side error!" });
+  }
+};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.json({ success: false, message: "All fields are required!" });
+  try {
+    const user = await User.findOne({ email });
+    if(!user) return res.json({success: false, message: "Wrong credentials!"});
+    const correctPass = await bcrypt.compare(password, user.password);
+    if (!correctPass)
+      return res.json({ success: false, message: "Wrong credentials!" });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.json({success: true,
+        name: user.name,
+        userEmail: email
+    })
+  } catch (error) {
+    return res.json({success: false, message: "Internal server error!"});
   }
 };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { login, signup } from "../controllers/auth.controller.js";
 const router = Router();
 router.post('/signup', signup);
+router.post('/login', login)
 
 
 

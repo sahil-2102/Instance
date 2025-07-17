67d4f9e39091a088044a6c85ba808319bbcba22b
Sahil <sk4386840@gmail.com>
2025-07-17 11:00:11 +0530

    configure database code and update main file

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
 dotenv.config();
-
+connectDB();
 const app = express();
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 app.use(cookieParser());
 const port = process.env.PORT || 5000
 

d371cd7823ee3702e43c69fcf0990f0eca79f04c
Sahil <sk4386840@gmail.com>
2025-07-17 14:26:44 +0530

    create protected route

---
+++ server/middlewares/protect.middleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const protectedRoute = async (req, res) => {
+    try {
+        const token = req.cookies?.token;
+        if(!token){
+            return res.status(400).json({success:false, message:"Unauthorized user!"});
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(!decoded){
+            return res.status(400).json({success:false, message:"Not Authorized!"});
+        }
+        req.user = decoded.id
+        next()
+    } catch (error) {
+        console.log(`Error in protected middleware: ${error.message}`);
+        return res.status(500).json({success:false,message:"Internal server error!"});
+    }
+}
+export default protectedRoute;

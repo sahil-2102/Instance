1087e58aeb13eb7db80c5594aa164b9cbe48f66b
Sahil <sk4386840@gmail.com>
2025-07-18 13:10:00 +0530

    create function for sending message

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,23 @@
+import Message from "../models/message.model.js";
+
+export const sendMessage = async(req, res) => {
+    try {
+        const myId = req.user;
+        const {chat, text} = req.body;
+        if(!chat || !text) return res.status(400).json({success:false,message:"chat id or text missing!"});
+
+        const newMessage = await Message.create({
+            sender: myId,
+            text,
+            chat
+        });
+        const populatedMessage = await Message.findById(newMessage._id)
+        .populate("sender","-password")
+        .populate("chat");
+        return res.status(200).json({success:true,message:populatedMessage});
+    } catch (error) {
+        console.log(`Error in sendMessage controller: ${error.message}`);
+        return res.status(500).json({success:false,message:"Internal server error!"});
+    }
+}
+

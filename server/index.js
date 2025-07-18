0a8c9bfb4610849fec7c73c97282a62eab0c4341
Sahil <sk4386840@gmail.com>
2025-07-18 13:41:46 +0530

    update main file

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
+import charRoutes from './routes/chat.route.js';
+import messageRoutes from './routes/message.route.js';
 dotenv.config();
 connectDB();
 const app = express();
@@ -16,6 +18,8 @@ const port = process.env.PORT || 5000
 
 app.use('/api/auth',authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/chat',charRoutes);
+app.use('/api/message',messageRoutes);
 
 
 
